e42690d05969de4b93b8759a416aa0d25738c50a
Albert <dony.nyc@gmail.com>
2023-02-28 13:28:10 +0400

    new animation

--- js/bannerAnimation.js
+++ js/bannerAnimation.js
@@ -1,6 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const fishes = document.querySelectorAll('.banner-bg-animation-img');
-    fishes[1].style.display = 'none';
+    const fishes = document.querySelectorAll('.bannerbg-animation-frames-2');
+    console.log(window.innerWidth);
+
+    fishes.forEach(el => {
+        if (window.innerWidth > 991) {
+            el.style.display = 'block';
+            const fishesWidth = el.clientWidth;
+            console.log(fishesWidth);
+            el.animate([
+                {
+                    transform: `translateX(${-fishesWidth * 2}px)`,
+                },
+                {
+                    transform: `translateX(${fishesWidth * 4}px)`,
+                },
+            ], {
+                duration: 25000,
+                iterations: 3
+            });
+        }
+    })
+})
+
+
+    // const fishes = document.querySelectorAll('.banner-bg-animation-img');
+    // fishes[1].style.display = 'none';
 
 
     // function animateFish2(time) {
@@ -33,77 +57,77 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-    const action = function () {
+    // const action = function () {
 
-        const start = fishes[0].clientWidth;
+    //     const start = fishes[0].clientWidth;
 
-        fishes.forEach(el => {
-            el.style.transform = `translateX(${-start}px)`;
-        });
+    //     fishes.forEach(el => {
+    //         el.style.transform = `translateX(${-start}px)`;
+    //     });
 
-        function animateFish1(time) {
-            fishes[0].animate([
-                {
-                    transform: `translate(${-start}px, 40px)`,
-                    filter: 'opacity(60%)',
-                },
-                {
-                    transform: `translate(${-start * 0.33}px, 0px)`,
-                    filter: 'opacity(80%)',
-                },
-                {
-                    transform: `translate(0px, 5px)`,
-                    filter: 'opacity(100%)',
-                },
-                {
-                    transform: `translate(0px, 10px)`,
-                    filter: 'opacity(80%)',
-                },
-                {
-                    transform: `translate(0px, 10px)`,
-                    filter: 'opacity(0%)',
-                },
-            ], {
-                duration: time,
-                iterations: 1
-            });
-        }
+    //     function animateFish1(time) {
+    //         fishes[0].animate([
+    //             {
+    //                 transform: `translate(${-start}px, 40px)`,
+    //                 filter: 'opacity(60%)',
+    //             },
+    //             {
+    //                 transform: `translate(${-start * 0.33}px, 0px)`,
+    //                 filter: 'opacity(80%)',
+    //             },
+    //             {
+    //                 transform: `translate(0px, 5px)`,
+    //                 filter: 'opacity(100%)',
+    //             },
+    //             {
+    //                 transform: `translate(0px, 10px)`,
+    //                 filter: 'opacity(80%)',
+    //             },
+    //             {
+    //                 transform: `translate(0px, 10px)`,
+    //                 filter: 'opacity(0%)',
+    //             },
+    //         ], {
+    //             duration: time,
+    //             iterations: 1
+    //         });
+    //     }
 
-        function animateFish3(time) {
-            fishes[2].animate([
-                {
-                    transform: `translate(${-start}px, 80px)`,
-                    filter: 'opacity(50%)',
-                },
-                {
-                    transform: `translate(${-start * 0.5}px, 40px)`,
-                    filter: 'opacity(80%)',
-                },
-                {
-                    transform: `translate(${-start * 0.03}px, 0px)`,
-                    filter: 'opacity(100%)',
-                },
-                {
-                    transform: `translate(0px, 3px)`,
-                    filter: 'opacity(80%)',
-                },
-                {
-                    transform: `translate(0px, 3px)`,
-                    filter: 'opacity(0%)',
-                },
-            ], {
-                duration: time,
-                iterations: 1
-            });
-        }
+    //     function animateFish3(time) {
+    //         fishes[2].animate([
+    //             {
+    //                 transform: `translate(${-start}px, 80px)`,
+    //                 filter: 'opacity(50%)',
+    //             },
+    //             {
+    //                 transform: `translate(${-start * 0.5}px, 40px)`,
+    //                 filter: 'opacity(80%)',
+    //             },
+    //             {
+    //                 transform: `translate(${-start * 0.03}px, 0px)`,
+    //                 filter: 'opacity(100%)',
+    //             },
+    //             {
+    //                 transform: `translate(0px, 3px)`,
+    //                 filter: 'opacity(80%)',
+    //             },
+    //             {
+    //                 transform: `translate(0px, 3px)`,
+    //                 filter: 'opacity(0%)',
+    //             },
+    //         ], {
+    //             duration: time,
+    //             iterations: 1
+    //         });
+    //     }
 
-        animateFish1(3500);
-        setTimeout(animateFish3, 3000, 3300);
-    }
+    //     animateFish1(3500);
+    //     setTimeout(animateFish3, 3000, 3300);
+    // }
 
-    action();
-    setInterval(() => {
-        action();
-    }, 6000);
+    // action();
+    // setInterval(() => {
+    //     action();
+    // }, 6000);
 
-})
+// })
