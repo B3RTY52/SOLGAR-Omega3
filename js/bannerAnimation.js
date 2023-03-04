09cbbf8a3107632ab553b8eb46e1298e49280dd4
Albert <dony.nyc@gmail.com>
2023-03-04 23:19:02 +0400

    animation sizes changed

--- js/bannerAnimation.js
+++ js/bannerAnimation.js
@@ -3,21 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(window.innerWidth);
 
     fishes.forEach(el => {
-        if (window.innerWidth > 991) {
-            el.style.display = 'block';
-            const fishesWidth = el.clientWidth;
-            console.log(fishesWidth);
-            el.animate([
-                {
-                    transform: `translateX(${-fishesWidth * 2}px)`,
-                },
-                {
-                    transform: `translateX(${window.innerWidth + fishesWidth}px)`,
-                },
-            ], {
-                duration: 30000,
-                iterations: Infinity
-            });
-        }
+        el.style.display = 'block';
+        const fishesWidth = el.clientWidth;
+        console.log(fishesWidth);
+        el.animate([
+            {
+                transform: `translateX(${-fishesWidth * 2}px)`,
+            },
+            {
+                transform: `translateX(${window.innerWidth + fishesWidth}px)`,
+            },
+        ], {
+            duration: 32000,
+            iterations: Infinity
+        });
     })
 })
